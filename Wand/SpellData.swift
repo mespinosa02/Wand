3c3a2efcde5b5e3b5b59eeea51061c0eace6b248
Michelle Espinosa <michelle.l.espinosa@gmail.com>
2020-04-21 01:02:46 -0700

    video 1

---
+++ Wand/SpellData.swift
@@ -0,0 +1,14 @@
+//
+//  SpellData.swift
+//  Wand
+//
+//  Created by SBAUser on 4/21/20.
+//  Copyright © 2020 Michelle Espinosa. All rights reserved.
+//
+
+import Foundation
+
+struct SpellData {
+    var name:String
+    
+}
--- Wand/ViewController.swift
+++ Wand/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var spells = ["Accio Dittany", "Accio Firebolt", "Accio Locket"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
 }
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        spells.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = spells[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
