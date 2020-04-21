dbea7ed8852442476dbd5aec52ef02b2efc63b47
Michelle Espinosa <michelle.l.espinosa@gmail.com>
2020-04-21 00:44:04 -0700

    Initial Commit

---
+++ Wand/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wand
+//
+//  Created by SBAUser on 4/21/20.
+//  Copyright © 2020 Michelle Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
